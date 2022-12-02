df6bb052608e378003f0a404c14aa6504ec1b502
jpmanti <jpmanti@gmail.com>
2022-12-02 15:15:23 -0500

    no register in main id

--- identification/urls.py
+++ identification/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 app_name='id'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='identification/login_register.html'),name="id"),
-    
+    path('reg', TemplateView.as_view(template_name='identification/register.html'),name="idreg"),
 ]
